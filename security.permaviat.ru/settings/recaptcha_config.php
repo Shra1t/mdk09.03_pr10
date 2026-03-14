d89ae58ed8779eb77cc46c6fe4c9ce1dce6b1b24
Габбасов <Flacko2018@mail.ru>
2026-03-14 23:04:28 +0500

    Правка ключа для капчи

--- security.permaviat.ru/settings/recaptcha_config.php
+++ security.permaviat.ru/settings/recaptcha_config.php
@@ -4,6 +4,6 @@
  * Шаг 2-3: Получить ключи на https://www.google.com/recaptcha/admin/create
  * Указать домены сайта, принять условия.
  */
-define('RECAPTCHA_SITE_KEY', '6LeYeYosAAAAADu2Y1oIABHJ0InXlylbUwLfQeTW');   // для виджета на странице (data-sitekey)
+define('RECAPTCHA_SITE_KEY', '6LeYeYosAAAAADu2Y1olABHJ0InXlylbUwLfQeTW');   // для виджета на странице (data-sitekey)
 define('RECAPTCHA_SECRET_KEY', '6LeYeYosAAAAAODagk4LrbMYkX9Z1D4W2r4VeY6j'); // для проверки на сервере
 ?>
